d513fc518a1cca49106cf68255442ee410604771
LisaSkalon <36557899+LisaSkalon@users.noreply.github.com>
2025-02-20 17:06:02 +0300

    Add files via upload

---
+++ RNAseq_analysis/Zoox_identification/kraken2.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#################################################################################################################
+#SBATCH --job-name=sbatchTemplate ## Name of your job
+#SBATCH --ntasks=16 ## number of cpu's to allocate for a job
+#SBATCH --ntasks-per-node=16 ## number of cpu's to allocate per each node
+#SBATCH --nodes=1 ## number of nodes to allocate for a job
+#SBATCH --mem=128G ## memory to allocate for your job in MB
+#SBATCH --time=1-00:00:00 ## time to allocate for your job in format: DD-HH:MM:SS
+#SBATCH --error=%J.errors ## stderr file name(The %J will print job ID number)
+#SBATCH --output=%J.output ## stdout file name(The %J will print job ID number)
+#SBATCH --mail-type=NONE ## Send your job status via e-mail: Valid type values are NONE, BEGIN, END, FAIL, REQUEUE, ALL
+########### Job information #############
+echo "================================"
+echo "Start at `date`"
+echo "Job id is $SLURM_JOBID"
+echo "Running on hosts: $SLURM_NODELIST"
+echo "Running on $SLURM_NNODES nodes."
+echo "Running on $SLURM_NTASKS processors."
+echo "================================"
+#########################################
+
+######## Load required modules ##########
+#. /etc/profile.d/modules.sh # Required line for modules environment to work
+#module load openmpi/1.8.4 python/2.7 # Load modules that are required by your program
+#########################################
+source /lustre1/home/mass/eskalon/miniconda/bin/activate kraken
+### Below you can enter your program job command ###
+
+for R1 in /lustre1/home/mass/eskalon/Porites/analysis/mapping/unaligned/*_Unmapped.out.mate1; do
+R2="${R1%_Unmapped.out.mate1}_Unmapped.out.mate2"
+R3="${R1%_Unmapped.out.mate1}_kraken.out"
+
+kraken2 --db /lustre1/home/mass/eskalon/kraken/database/core \
+ --paired  --threads 16 \
+$R1 $R2 > $R3
+
+done
