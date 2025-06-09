5f9a078c48fccad47c6eaafd21490a67a385a8b8
LisaSkalon <36557899+LisaSkalon@users.noreply.github.com>
2025-06-09 12:52:48 +0300

    Update diagnostics.R

--- RNAseq_analysis/Connectivity/diagnostics.R
+++ RNAseq_analysis/Connectivity/diagnostics.R
@@ -41,3 +41,33 @@ pdf("Diag_plots.pdf", height=20, width=15)
 theme_set(theme_gray(base_size = 18))
 grid.arrange(QD, DP, FS, MQ, MQRankSum, SOR, ReadPosRankSum, nrow=4)
 dev.off()
+
+# Plots:
+#	QUAL - variant confidence QUAL score (based on all reads)
+#	QD - variant confidence standardized by depth.
+#		- https://gatk.broadinstitute.org/hc/en-us/articles/360056968272-QualByDepth
+
+# 	DP - *combined* depth per SNP across samples
+#		- https://gatk.broadinstitute.org/hc/en-us/articles/360057441391-DepthPerSampleHC
+#		- Depth of informative reads supporting variance (i.e. reads that show strong and clear evidence of the variant)
+
+#	MQ - mapping quality of a SNP.
+#		- https://gatk.broadinstitute.org/hc/en-us/articles/360057438331-RMSMappingQuality
+#		- Overall mapping quality of reads supporting a variant call, averaged over all samples in a cohort.
+
+#	FS - strand bias in support for REF vs ALT allele calls
+#		- https://gatk.broadinstitute.org/hc/en-us/articles/360056968392-FisherStrand
+#		- The output is a Phred-scaled p-value. The higher the output value, the more likely there is to be bias. More bias is indicative of false positive calls.
+
+#	SOR - sequencing bias in which one DNA strand is favored over the other 
+#		- https://gatk.broadinstitute.org/hc/en-us/articles/360056967852-AS-StrandOddsRatio
+#		- Allele-specific strand bias estimated by the Symmetric Odds Ratio test.
+ 
+#	MQRankSum - Rank sum test for mapping qualities of REF vs. ALT reads.
+#		- https://gatk.broadinstitute.org/hc/en-us/articles/360057439151-MappingQualityRankSumTest
+#		- Compares the mapping qualities of the reads supporting the reference allele with those supporting the alternate allele
+
+#	ReadPosRankSum - do all the reads support a SNP call tend to be near the end of a read.
+#		- https://gatk.broadinstitute.org/hc/en-us/articles/360057439971-AS-ReadPosRankSumTest
+#		- Allele-specific Rank Sum Test for relative positioning of REF versus ALT allele within reads.
+#		- Tests whether there is evidence of bias in the position of alleles within the reads that support them, between the reference and each alternate allele. 
